b231dd045f03fe7a3aa659168da3012ed928b873
carlitosvi <carlitosvi@yahoo.com>
2013-06-13 03:28:59 -0500

    Update urls.py

--- fileupload/urls.py
+++ fileupload/urls.py
@@ -1,9 +1,31 @@
 from django.conf.urls.defaults import *
-from fileupload.views import PictureCreateView, PictureDeleteView
+from fileupload.views import PictureCreateView, PictureDeleteView #after here i add it this
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
 
+admin.autodiscover()
 
 urlpatterns = patterns('',
 
     (r'^new/$', PictureCreateView.as_view(), {}, 'upload-new'),
-    (r'^delete/(?P<pk>\d+)$', PictureDeleteView.as_view(), {}, 'upload-delete'),
+    (r'^delete/(?P<pk>\d+)$', PictureDeleteView.as_view(), {}, 'upload-delete'),#i add this here!
+    
+     # Examples:
+    # url(r'^$', 'upload.views.home', name='home'),
+    url(r'^upload/', include('fileupload.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+
+
+   
+
+import os
+urlpatterns += patterns('',
+        (r'^media/(.*)$', 'django.views.static.serve', {'document_root': os.path.join(os.path.abspath(os.path.dirname(__file__)), 'media')}),
 )
